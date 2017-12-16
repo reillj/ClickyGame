bccbceac6d5c0e389b88fc37223b63fcde5a33fc
reillj <jenniferreilly1992@gmail.com>
2017-12-16 12:25:46 -0700

    Add files via upload

---
+++ src/components/app/App.test.js
@@ -0,0 +1,9 @@
+ (7 sloc)  208 Bytes
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+});
---
+++ src/components/bootstrap/body.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+
+class Body extends Component {
+  render(props){
+    return(
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-3">
+                        <img src="./images/Alter-egos-nate-in-lab-coat.png" className="img-thumbnail" id="img"></img>
+                    </div>
+                    <div className="col-md-3">
+                        <img src="./images/Blanketjefferson.png" className="img-thumbnail" id="img"></img>
+                    </div>
+                    <div className="col-md-3">
+                        <img src="./images/ButtersStotch.png" className="img-thumbnail" id="img"></img>
+                    </div>
+                    <div className="col-md-3">
+                        <img src="./images/Cornwallis-hankey.png" className="img-thumbnail" id="img"></img>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-3">
+                        <img src="./images/Fastpass-00.png" className="img-thumbnail" id="img"></img>
+                    </div>
+                    <div className="col-md-3">
+                        <img src="./images/FerrariRaisins.png" className="img-thumbnail" id="img"></img>
+                    </div>
+                    <div className="col-md-3">
+                        <img src="./images/Gordon-stoltski.png" className="img-thumbnail" id="img"></img>
+                    </div>
+                    <div className="col-md-3">
+                        <img src="./images/Kyle-nohat.transparent.png" className="img-thumbnail" id="img"></img>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-3">
+                        <img src="./images/New-heidi.png" className="img-thumbnail" id="img"></img>
+                    </div>
+                    <div className="col-md-3">
+                        <img src="./images/Princess-kenny.png" className="img-thumbnail" id="img"></img>
+                    </div>
+                    <div className="col-md-3">
+                        <img src="./images/The-coon.png" className="img-thumbnail" id="img"></img>
+                    </div>
+                    <div className="col-md-3">
+                        <img src="./images/Toolshed.transparent.png" className="img-thumbnail" id="img"></img>
+                    </div>
+                </div>
+            </div>
+      )   
+  }
+}
+
+
+export default Body;
